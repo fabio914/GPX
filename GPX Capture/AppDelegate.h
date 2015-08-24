43be16eb9773d764096b53b8f7bc66bfbf372c95
Fabio Dela Antonio <fabio@bikedafirma.com>
2015-08-24 14:17:16 -0300

    Initial Commit

---
+++ GPX Capture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPX Capture
+//
+//  Created by Fabio on 8/24/15.
+//  Copyright (c) 2015 Bluenose. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
